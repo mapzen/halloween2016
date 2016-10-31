dfbce516f65876875ad83e69edf46564925aa284
souperneon <ekta.daryanani@gmail.com>
2016-10-31 10:51:22 -0400

    removing year and location type

--- js/mapzen-horror-road-trip.js
+++ js/mapzen-horror-road-trip.js
@@ -1,72 +1,56 @@
 var horrorArr = [
    {
       "movie":"The Blair Witch Project",
-      "year": "1999",
-      "location":"Cemetary",
-      "location-type":"Cemetary",
+      "location":"Burkittsville Cemetery",
       "lat":"39.394085",
       "lng":"-77.6301155",
       "address":"Burkittsville Cemetery, Burkittsville, MD 21718"
    },
    {
       "movie":"The Exorcist",
-      "year": "1973",
       "location":"Regan's house",
-      "location-type":"House",
       "lat":"38.9056951",
       "lng":"-77.0726135",
       "address":"3600 Prospect Street, Georgetown, Washington DC 20007"
    },
    {
       "movie":"Dawn of the Dead",
-      "year": "1978",
       "location":"The Monroeville Mall",
-      "location-type":"Mall",
       "lat":"40.42905",
       "lng":"-79.7973767",
       "address":"The Monroeville Mall, 200 Mall Cir Dr, Monroeville, PA 15146"
    },
    {
       "movie":"The Silence of the Lambs",
-      "year": "1991",
       "location":"Holding cell",
-      "location-type":"University building9",
       "lat":"40.4448878",
       "lng":"-79.958479",
       "address":"Allegheny County Soldiers and Sailors Memorial Hall, 4141 Fifth Ave, Pittsburgh, PA 15213"
    },
    {
       "movie":"The Night of the Living Dead",
-      "year": "1968",
       "location":"The Evans City Cemetery",
-      "location-type":"Cemetary",
       "lat":"40.7620932",
       "lng":"-80.0647115",
       "address":"Evans City Cemetery, Franklin Rd, Evans City, PA 16033"
    },
    {
       "movie":"Friday the 13th",
-      "year": "1980",
-      "location":"Camp Blood (Camp Crystal Lake)",
-      "location-type":"Boy scouts camp",
+      "location":"Camp Blood (Camp Crystal Lake)",,
       "lat":"41.0559803",
       "lng":"-74.951018",
       "address":"Camp No-Be-Bo-Sco, 11 Sand Pond Rd, Hardwick, NJ 07825"
    },
    {
       "movie":"The Amityville Horror",
-      "year": "1979",
-      "location":"House",
-      "location-type":"House",
+      "location":"The possesed house",
       "lat":"39.950315",
       "lng":"-74.1900353",
       "address":"18 Brooks Rd, Toms River, NJ 08753"
    },
    {
       "movie":"Rosemary’s Baby",
-      "year": "1968",
-      "location":"The Bramford",
-      "location-type":"Apartment building",
+      "location":"The Bramford building",
       "lat":"40.7765258",
       "lng":"-73.9783295",
       "address":"The Dakota, 1 W 72nd St, New York, NY 10023"
